4eb0572d20cce00b7bdb9131d38cdc94dc44e23e
Jason Diaz Jimenez <developervulpo@outlook.com>
2022-03-26 15:50:41 -0500

    Database Setup

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,4 +1,7 @@
 import fastify from "fastify";
+import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const app = fastify({ logger: true });
 const port = process.env.PORT || 3000;
@@ -8,6 +11,16 @@ app.register(require("./routes/fursona"), { prefix: "/fursona" });
 app.get("/", (_request, reply) => {
   reply.send({ hello: "world" });
 });
+
+// Conntacts to mongoDB
+mongoose
+  .connect("mongodb://localhost:27017/fursona")
+  .then(() => {
+    console.log("Connected to mongoDB");
+  })
+  .catch((err) => {
+    console.log("Error connecting to mongoDB: ", err);
+  });
 
 app.listen(port, (err, address) => {
   if (err) {
