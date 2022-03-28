032e34fe60e3f5166830f74622d145fcf90d4e19
Jason Diaz Jimenez <developervulpo@outlook.com>
2022-03-28 04:35:13 -0500

    Made Fursona Gallery Item component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Container from "../components/Container";
+import GalleryItem from "../components/GalleryItem";
 import styles from "../styles/Home.module.scss";
 
 export default function Home() {
@@ -29,6 +30,13 @@ export default function Home() {
               <h2 id={styles["label-popular"]}>Popular Fursonas</h2>
               <div className="fursona-gallery-grid">
                 {/* TODO: Convert gallery items into components */}
+                <GalleryItem
+                  avatar="https://cdn.discordapp.com/avatars/852070153804972043/11c0da7b2aa7d852310b6f6bb18edf5e.png?size=4096"
+                  gradientCSS="linear-gradient(200.64deg, #FF00F5 13.68%, rgba(255, 0, 245, 0.54375) 55.98%, #FF5C00 130.37%);"
+                  name="Ozzy"
+                  species="Otter"
+                  primaryColor="black"
+                />
                 <div className="fursona-gallery-item"></div>
                 <div className="fursona-gallery-item"></div>
                 <div className="fursona-gallery-item"></div>
