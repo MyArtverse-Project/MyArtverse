10b66547776b5650efceb83f249d9264f8526f43
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-03-30 21:09:53 +0800

    Jaker proposed the '💖' on popular sect
    minor fixes wow

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -27,12 +27,12 @@ export default function Home() {
         <main>
           <div id={styles["fursona-main-showcase"]}>
             <section>
-              <h2 id={styles["label-popular"]}>Popular Fursonas</h2>
+              <h1 id={styles.labelPopular}>Popular Fursonas</h1>
               <div className="fursona-gallery-grid">
                 {/* TODO: Convert gallery items into components */}
                 <GalleryItem
                   avatar="https://cdn.discordapp.com/avatars/852070153804972043/11c0da7b2aa7d852310b6f6bb18edf5e.png?size=4096"
-                  gradientCSS="linear-gradient(200.64deg, #FF00F5 13.68%, rgba(255, 0, 245, 0.54375) 55.98%, #FF5C00 130.37%)"
+                  gradientCSS="linear-gradient(200.64deg, #ff00f5 13.68%, rgba(255, 0, 245, 0.54375) 55.98%, #FF5C00 130.37%)"
                   name="Ozzy"
                   species="Otter"
                   primaryColor="black"
@@ -45,7 +45,7 @@ export default function Home() {
               </div>
             </section>
             <section>
-              <h2 id={styles["label-new"]}>New Fursonas</h2>
+              <h1 id={styles.labelNew}>New Fursonas</h1>
               <div className="fursona-gallery-grid">
                 {/* TODO: Convert gallery items into components */}
                 <div className="fursona-gallery-item"></div>
@@ -60,5 +60,5 @@ export default function Home() {
         <footer></footer>
       </Container>
     </div>
-  );
+  )
 }
