ecb9ef51826f40700133d89e937b21e74db67dc3
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-04-03 14:04:16 +0800

    Improved skeletion animation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,23 @@
-import React from "react";
-import styles from "../styles/Header.module.scss";
+import React from "react"
+import styles from "../styles/Header.module.scss"
 
 export default function Header() {
-    return (
-        <header>
-            <div id={styles["wrapper"]}>
-                <strong id={styles["logo"]}>MyFursona</strong>
-                <div id={styles["search-box-wrapper"]}>
-                    <div id={styles["search-box"]}>
-                        <input type="search" placeholder="Search fursonas..."/>
-                    </div>
-                </div>
-                <div id={styles["user-actions"]}>
-                    {/* SVG logos here for logged in users */}
+  return (
+    <header>
+      <div id={styles["wrapper"]}>
+        <strong id={styles["logo"]}>MyFursona</strong>
+        <div id={styles["search-box-wrapper"]}>
+          <div id={styles["search-box"]}>
+            <input type="search" placeholder="Search fursonas..." />
+          </div>
+        </div>
+        <div id={styles["user-actions"]}>
+          {/* SVG logos here for logged in users */}
 
-                    {/* Display somemthing here for logged out users */}
-                    <button>Login</button>
-                </div>
-            </div>
-        </header>
-    );
+          {/* Display somemthing here for logged out users */}
+          <button>Login</button>
+        </div>
+      </div>
+    </header>
+  )
 }
