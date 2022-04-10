f948c22b95ba0e8393002365c17acceed76b313e
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-04-10 13:39:49 +0800

    finished user skeleton

--- src/pages/user.tsx
+++ src/pages/user.tsx
@@ -6,14 +6,13 @@ export default function User() {
     <Container>
       <div id={styles["user-container"]}>
         <div id={styles["user-profile"]}>
-          <div id={styles["avatar-skeleton"]}></div>
-          <div id={styles["species-skeleton"]}></div>
-          <div id={styles["social-skeleton"]}></div>
+          <div id={styles["skeleton-avatar"]}></div>
+          <div id={styles["skeleton-social"]}></div>
         </div>
         <div id={styles["user-info"]}>
-          <div id={styles["name-skeleton"]}></div>
-          <div id={styles["bio-skeleton"]}></div>
-          <div id={styles["species-skeleton"]}></div>
+          <div id={styles["skeleton-name"]}></div>
+          <div id={styles["skeleton-species"]}></div>
+          <div id={styles["skeleton-bio"]}></div>
         </div>
       </div>
     </Container>
