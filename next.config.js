5b59c094a7c766bf8b1bd290cf9545012f4c027f
Kerby Keith Aquino <skepfoosky15@gmail.com>
2022-04-17 10:11:44 +0800

    feat: added PWA support
    - Updated manifest.json and added icons from Figma Balls
    
    - Update README.md

--- next.config.js
+++ next.config.js
@@ -4,3 +4,13 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
+const PWASupport = require("next-pwa");
+
+module.exports = PWASupport({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  },
+});
