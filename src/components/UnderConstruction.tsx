b34c562e0ce79d2b6a36160ef10ed7b754fa59c3
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-04-28 00:34:26 +0800

    Added under construction component

---
+++ src/components/UnderConstruction.tsx
@@ -0,0 +1,21 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faExclamation, faExclamationTriangle } from "@fortawesome/free-solid-svg-icons"
+import Link from "next/link"
+import styles from "../styles/WIP.module.scss"
+
+export default function UnderConstruction() {
+  return (
+    <div id={styles["under-construction-notice"]}>
+      <FontAwesomeIcon icon={faExclamationTriangle} size="2x" />
+      <h2>Under construction</h2>
+      <p>
+        This site is currently under construction. <i>MyFursona</i> is an
+        open-source project and help us out by contributing the project on{" "}
+        <Link href="https://github.com/MyFursona-Project/MyFursona" passHref>
+          <a target="_blank">GitHub</a>
+        </Link>
+        !
+      </p>
+    </div>
+  )
+}
