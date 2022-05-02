4ecd2d11f94625038fee8196df809dea2a9253d5
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-05-02 15:50:58 +0800

    Squashed commit of the following:
    commit e9ec218bbabf097cf3f609a331fe002f8b6fa719
    Author: Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
    Date:   Mon May 2 15:24:34 2022 +0800
    
        added the cool card animation thing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,49 @@
-import Link from "next/link";
-import Container from "../components/Container";
-import GalleryItem, { LoadingGalleryItem } from "../components/GalleryItem";
-import styles from "../styles/Home.module.scss";
-import { useEffect, useState } from "react";
-import UnderConstruction from "../components/UnderConstruction";
+import React from "react"
+import Link from "next/link"
+import Container from "../components/Container"
+import GalleryItem, { LoadingGalleryItem } from "../components/GalleryItem"
+import styles from "../styles/Home.module.scss"
+import { useEffect, useState } from "react"
+import UnderConstruction from "../components/UnderConstruction"
 
 export default function Home() {
   const [data, setData] = useState(null)
   const [loading, setLoading] = useState(true)
   useEffect(() => {
     // ! Disabled temporarily for debugging front-end stuff
-    // const fetchFursona = async () => {
+    // const fetchFursona = (async () => {
     //   const res = await fetch(`api/fursona/popular`)
     //   const fursonas = await res.json()
     //   setLoading(false)
     //   return setData(fursonas)
-    // }
-    // fetchFursona()
+    // })();
   }, [])
   return (
     <Container>
       <main>
         <UnderConstruction />
+        <div id={styles["header-hero"]}>
+          <article>
+            <h1>Manage, store, and show your fursonas in one place</h1>
+            <p>
+              <strong>MyFursona</strong> is an open-source platform where you can show off
+              your fluffy characters and show them off to your friends!
+            </p>
+          </article>
+          <div id={styles["hero-animation"]}>
+            <div className={styles["image-folder-overlay"]}>
+              <div className={styles['image-folder-item']} style={{'--stagger': '1'} as React.CSSProperties }>
+                <img src="/images/ozzy.png" alt="Ozzy Fursona" />
+              </div>
+              <div className={styles['image-folder-item']} style={{'--stagger': '2'} as React.CSSProperties }>
+                <img src="/images/ozzy.png" alt="Ozzy Fursona" />
+              </div>
+              <div className={styles['image-folder-item']} style={{'--stagger': '3'} as React.CSSProperties }>
+                <img src="/images/ozzy.png" alt="Ozzy Fursona" />
+              </div>
+            </div>
+          </div>
+        </div>
         <div id={styles["fursona-main-showcase"]}>
           <section>
             <h2 id={styles.labelPopular}>Popular Fursonas</h2>
@@ -36,7 +58,7 @@ export default function Home() {
                       "linear-gradient(228.09deg, #AB41FF 0%, #FF248D 100%)"
                     }
                     primaryColor={"black"}
-                    />
+                  />
                 </a>
               </Link>
               <LoadingGalleryItem />
@@ -48,9 +70,6 @@ export default function Home() {
           <section>
             <h2 id={styles.labelNew}>New Fursonas</h2>
             <div className="fursona-gallery-grid">
-              <LoadingGalleryItem />
-              <LoadingGalleryItem />
-              <LoadingGalleryItem />
               <LoadingGalleryItem />
               <LoadingGalleryItem />
               <LoadingGalleryItem />
