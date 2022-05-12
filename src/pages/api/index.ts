155d27aa0f56d367ff1c7cbf2a24c32a8f6ded07
Jason Diaz Jimenez <developervulpo@outlook.com>
2022-05-12 06:54:13 -0500

    Moving api systems

--- src/pages/api/index.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next"
-import dbConnect from "../../utils/connectMongo"
-
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  res.json({})
-}
