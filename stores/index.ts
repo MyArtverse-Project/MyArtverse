df4ccca12260cdb6f62dca5ee9e9e76e99599000
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2023-02-05 20:25:41 +0800

    add profile pinia store

--- stores/index.ts
+++ stores/index.ts
@@ -17,3 +17,48 @@ export const useUserSettingsStore = defineStore("settings", () => {
     }
   }
 })
+
+/**
+ * Accent color of a user's profile
+ * @default "blue"
+ */
+type AccentColors =
+  | "orange"
+  | "yellow"
+  | "cyan"
+  | "blue"
+  | "green"
+  | "purple"
+  | "red"
+  | "gray"
+
+interface Fursona {
+  name: string
+  species: string
+  bio: string
+}
+
+interface ProfileStore {
+  accountName: string
+  accountStatus: string
+  accountCreated: Date | string
+  fursonas: Fursona[] & never[]
+  followers: number
+  following: number
+  profileAccentColor: AccentColors
+  isPrivateProfile: boolean
+  isSuspended: boolean
+  isBlocked: boolean
+}
+
+export const useProfileStore = defineStore("profile", () => {
+  state: (): Partial<ProfileStore> => {
+    return {
+      accountName: "",
+      accountStatus: "",
+      profileAccentColor: "blue",
+      fursonas: [],
+      isSuspended: false,
+    }
+  }
+})
