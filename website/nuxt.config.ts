652f2f50b193ae26aff2be1694b0cecfbb85e0b2
Kerby Keith Aquino <skepfoosky15@gmail.com>
2023-02-25 16:10:47 +0800

    remove msft clarity, still under devekionebt

--- website/nuxt.config.ts
+++ website/nuxt.config.ts
@@ -31,18 +31,6 @@ export default defineNuxtConfig({
 					crossorigin: "use-credentials",
 				},
 			],
-			script: [
-				{
-					type: "text/javascript",
-					children: `
-          (function(c,l,a,r,i,t,y){
-          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", "frdjdgrmiy");
-          `,
-				},
-			],
 		},
 	},
 	build: {
