aefa549d5bd872daa131f1973ed578a3d1ca51b5
Kerby Keith Aquino <skepfoosky15@gmail.com>
2023-07-21 19:36:51 +0800

    project cleanup, update PWA package

--- website/next.config.js
+++ website/next.config.js
@@ -1,16 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-// ! PWA package won't resolve something about webpack
-// const withPWA = require("@ducanh2912/next-pwa").default({
-//   dest: "public"
-// })
-
-// module.exports = withPWA({
-//   experimental: {
-//     typedRoutes: true
-//   }
-// })
-
 const nextConfig = {
   experimental: {
     typedRoutes: true
