3fb46c27ce553e1626d6974344d97cdb6e1a52cd
Kerby Keith Aquino <skepfoosky15@gmail.com>
2023-09-29 20:58:59 +0800

    eslint cleanup

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  extends: ["myfursona"],
+  settings: {
+    next: {
+      rootDir: ["apps/*/"]
+    }
+  }
+}
--- packages/eslint-config-myfursona/index.js
+++ packages/eslint-config-myfursona/index.js
@@ -1,7 +1,8 @@
-{
-  "extends": ["next/core-web-vitals"],
-  "plugins": ["unused-imports"],
-  "rules": {
+module.exports = {
+  extends: ["next", "turbo", "prettier"],
+  plugins: ["unused-imports"],
+  rules: {
+    "@next/next/no-html-link-for-pages": "off",
     "react/no-unescaped-entities": "off",
     "unused-imports/no-unused-imports": "warn",
     "@next/next/no-before-interactive-script-outside-document": "off"
