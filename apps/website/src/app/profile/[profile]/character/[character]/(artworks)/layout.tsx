0937c4d52663a53820e3efdd155a54b258d08253
Jason Diaz Jimenez <developervulpo@outlook.com>
2023-10-03 23:11:31 -0500

    Added Blank pages for characters + Plus page

---
+++ apps/website/src/app/profile/[profile]/character/[character]/(artworks)/layout.tsx
@@ -0,0 +1,18 @@
+import type { SlugRouteProps } from "@/types"
+import { MarginClamp } from "@/components/ui"
+
+
+export default function Layout({
+  params,
+  children
+}: SlugRouteProps & { children: React.ReactNode }) {
+  const profile = params.profile
+
+  return (
+    <>
+      <MarginClamp as="main">
+        <div className="py-4">{children}</div>
+      </MarginClamp>
+    </>
+  )
+}
---
+++ apps/website/src/app/profile/[profile]/character/[character]/(biography)/biography/page.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const data = {
+  character: ""
+}
+
+export default function Character() {
+  return <div>page contents</div>
+}
