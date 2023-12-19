92c16eee9404ae68df738d8b4131755e165c322b
Kerby Keith Aquino <skepfoosky15@gmail.com>
2023-12-19 16:39:13 +0800

    add path matcher in middleware.ts

--- apps/website/src/middleware.ts
+++ apps/website/src/middleware.ts
@@ -56,3 +56,16 @@ export default function middleware() {
 
   return response
 }
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico).*)"
+  ]
+}
