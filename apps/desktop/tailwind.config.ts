abcd3f8fda27cd4ce1c961e85cf1e35ffd62246f
Kerby Keith Aquino <skepfoosky15@gmail.com>
2024-02-21 16:01:49 +0800

    fix duplicate tw configs

--- apps/desktop/tailwind.config.ts
+++ apps/desktop/tailwind.config.ts
@@ -1,21 +1,3 @@
-import type { Config } from "tailwindcss"
-import myfursonaPreset from "@myfursona-internal/config/tailwind.config"
-import defaultTheme from "tailwindcss/defaultTheme"
+import twConfig from "@myfursona-internal/config/tailwind-client-only"
 
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "../../packages/ui/src/**/*.{ts,tsx}",
-    "../../node_modules/@myfursona/biro-ui/src/**/*.{ts,tsx}"
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        inter: ["Inter", ...defaultTheme.fontFamily.sans],
-        "open-sans": ["Open Sans", ...defaultTheme.fontFamily.sans]
-      }
-    }
-  },
-  presets: [myfursonaPreset]
-} satisfies Config
+export default twConfig
---
+++ packages/config/tailwind-client-only.ts
@@ -0,0 +1,21 @@
+import type { Config } from "tailwindcss"
+import myfursonaPreset from "@myfursona-internal/config/tailwind.config"
+import defaultTheme from "tailwindcss/defaultTheme"
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "../../packages/ui/src/**/*.{ts,tsx}",
+    "../../node_modules/@myfursona/biro-ui/src/**/*.{ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["Inter", ...defaultTheme.fontFamily.sans],
+        "open-sans": ["Open Sans", ...defaultTheme.fontFamily.sans]
+      }
+    }
+  },
+  presets: [myfursonaPreset]
+} satisfies Config
