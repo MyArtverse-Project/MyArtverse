c65dd76c1cebcced5410a66dbacd771f1e9e006c
Kerby Keith Aquino <skepfoosky15@gmail.com>
2024-03-04 18:26:46 +0800

    temp disable fetchdata because of errors

--- apps/website/src/app/(main)/layout.tsx
+++ apps/website/src/app/(main)/layout.tsx
@@ -5,12 +5,12 @@ export default async function MainLayout({
   children,
   modal
 }: Readonly<{ children: React.ReactNode; modal: React.ReactNode }>) {
-  const userData = await fetchUserData()
+  // const userData = await fetchUserData()
   return (
     <>
       <div>
         <header className="sticky top-0 z-20">
-          <Navbar user={userData} />
+          <Navbar />
           <Sidebar />
         </header>
         <div id="skip-nav" className="min-h-[calc(100dvh-3.75rem)]">
