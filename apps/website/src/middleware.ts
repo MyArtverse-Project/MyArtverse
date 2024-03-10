eaa7bbb1eadc8cb5a74991db211f0bf27075ba82
Kerby Keith Aquino <skepfoosky15@gmail.com>
2024-03-10 12:54:07 +0800

    enable csp rules only on production

--- apps/website/src/middleware.ts
+++ apps/website/src/middleware.ts
@@ -1,38 +1,40 @@
 import { type NextRequest, NextResponse } from "next/server"
+import { isProduction } from "./utils/env"
 import { generateCSP } from "./utils/generateCSP"
 
 export function middleware(request: NextRequest) {
-  // Content-Security-Policy
-  const nonce = Buffer.from(crypto.randomUUID()).toString("base64")
-  const requestHeaders = new Headers(request.headers)
+  // Enable these CSP rules on production and not on dev servers
+  if (isProduction) {
+    const nonce = Buffer.from(crypto.randomUUID()).toString("base64")
+    const cspRules = generateCSP({
+      "script-src": [
+        "self",
+        "unsafe-eval",
+        `nonce-${nonce}`,
+        "https://*.umami.is",
+        "https://hcaptcha.com",
+        "https://*.hcaptcha.com",
+        "https://www.clarity.ms"
+      ],
+      "img-src": ["self", "unsafe-eval", "https://image.ctfassets.net"],
+      "connect-src": ["self", "https://hcaptcha.com", "https://*.hcaptcha.com"],
+      "frame-ancestors": ["none"],
+      "upgrade-insecure-requests": true
+    })
 
-  const cspRules = generateCSP({
-    "script-src": [
-      "self",
-      "unsafe-eval",
-      `nonce-${nonce}`,
-      "https://*.umami.is",
-      "https://hcaptcha.com",
-      "https://*.hcaptcha.com",
-      "https://www.clarity.ms"
-    ],
-    "img-src": ["self", "unsafe-eval", "https://image.ctfassets.net"],
-    "connect-src": ["self", "https://hcaptcha.com", "https://*.hcaptcha.com"],
-    "frame-ancestors": ["none"],
-    "upgrade-insecure-requests": true
-  })
+    const requestHeaders = new Headers(request.headers)
+    requestHeaders.set("Content-Security-Policy", cspRules)
 
-  requestHeaders.set("Content-Security-Policy", cspRules)
+    const response = NextResponse.next({
+      request: {
+        headers: requestHeaders
+      }
+    })
 
-  const response = NextResponse.next({
-    request: {
-      headers: requestHeaders
-    }
-  })
+    response.headers.set("Content-Security-Policy", cspRules)
 
-  response.headers.set("Content-Security-Policy", cspRules)
-
-  return response
+    return response
+  }
 }
 
 export const config = {
