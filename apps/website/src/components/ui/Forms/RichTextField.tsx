460465ed5df5243f6f9b1d01080a4eddfd18fb29
Jason Diaz Jimenez <developervulpo@outlook.com>
2024-03-19 21:57:28 -0500

    Refactor RichTextField component and remove unused files

--- apps/website/src/components/ui/Forms/RichTextField.tsx
+++ apps/website/src/components/ui/Forms/RichTextField.tsx
@@ -1,3 +1,15 @@
-export default function RichTextField() {
-  return <textarea />
+interface IRichTextFieldProps {
+  inputName: string
+}
+
+export default function RichTextField({ inputName }: IRichTextFieldProps) {
+  return (
+    <div className="h-32 w-full space-y-2">
+      <span className="text-600 flex gap-x-0.5 font-bold uppercase">Bio</span>
+      <textarea
+        id="likes"
+        className="text-700 border-400 bg-100 h-32 w-full rounded-md border px-4 py-2"
+      />
+    </div>
+  )
 }
---
+++ apps/website/src/components/ui/Forms/SelectField.tsx
@@ -0,0 +1,24 @@
+import React from "react"
+
+interface ISelectFieldProps {
+  inputName: string
+  options: { value: string; label: string }[]
+}
+
+export default function SelectField({ inputName, options }: ISelectFieldProps) {
+  return (
+    <div className="flex w-full flex-col gap-y-1.5">
+      <span className="text-600 gap-x-0.5 font-bold uppercase ">{inputName}</span>
+      <select className="text-700 border-400 bg-100 rounded-md border px-4 py-2">
+        <option value="none" disabled>
+          {inputName}
+        </option>
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    </div>
+  )
+}
