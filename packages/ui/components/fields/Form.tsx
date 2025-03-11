ba7b0d75a61669502fe2189d3a2e625f9ef2c62b
Kerby Keith Aquino <hello@kurojifusky.com>
2025-03-11 19:28:23 +0800

    minor: use `cn` util to merge tw classes

--- packages/ui/components/fields/Form.tsx
+++ packages/ui/components/fields/Form.tsx
@@ -1,4 +1,4 @@
-import type { PropsWithChildren } from "react"
+import { cn } from "@mav/shared/utils"
 
 export interface FormState {
   errors: Record<string, string[]>
@@ -11,12 +11,12 @@ interface FormProps {
   action?: (formData: FormData) => void | string
 }
 
-export function Form(props: PropsWithChildren<FormProps>) {
+export function Form(props: React.PropsWithChildren<FormProps>) {
   return (
     <form
       data-mav-form-wrapper=""
       onSubmit={props.onSubmit}
-      className={`flex flex-col gap-y-2.5 ${props.className}`}
+      className={cn("flex flex-col gap-y-2.5", props.className)}
       // @ts-expect-error: Temporary fix for the action prop
       action={props.action}
     >
