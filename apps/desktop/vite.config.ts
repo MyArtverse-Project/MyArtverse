11dec1b35189965e055a391e327fd44e71b629ae
Kerby Keith Aquino <hello@kurojifusky.com>
2025-03-17 21:48:07 +0800

    chore(deps): add `@tailwindcss/vite` package

--- apps/desktop/vite.config.ts
+++ apps/desktop/vite.config.ts
@@ -1,8 +1,9 @@
+import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig(async () => ({
-  plugins: [react()],
+  plugins: [tailwindcss(), react()],
   clearScreen: false,
   server: {
     port: 1420,
