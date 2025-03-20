0a884a763eb9b7694c1599f1eb73d1d417c543ee
Kerby Keith Aquino <hello@kurojifusky.com>
2025-03-20 11:29:54 +0800

    chore: added helper type for async routes
    code copied from https://github.com/kuroji-fusky/SponsorExplorer/blob/29e120a/client/src/types/routeParams.ts#L3-L17

--- apps/web/src/app/(profile)/profile/[handle]/layout.tsx
+++ apps/web/src/app/(profile)/profile/[handle]/layout.tsx
@@ -1,12 +1,18 @@
 import AppLayout from "@/components/layouts/AppLayout/AppLayout"
 import { ProfileMasthead } from "@/components/layouts/Mastheads"
+import { DefineRouteParams } from "@/types"
 import { fetchUser, fetchUserData } from "@/utils/api"
 
-export default async function MainProfileLayout(props: React.PropsWithChildren) {
-  // @ts-expect-error
+type AsyncProps = DefineRouteParams<{ handle: string }>
+
+export default async function MainProfileLayout(
+  props: React.PropsWithChildren & AsyncProps,
+) {
   const { handle } = await props.params
+
   const self = await fetchUserData()
   const user = handle === self.handle ? self : await fetchUser(handle)
+
   return (
     <AppLayout>
       <ProfileMasthead
