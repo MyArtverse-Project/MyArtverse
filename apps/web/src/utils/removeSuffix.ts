8cd643579bac81ca0402166176e607e180e71603
Jason Diaz Jimenez <ozzythedev@outlook.com>
2025-06-26 21:35:00 -0500

    fix: removeSuffix for auth forms

--- apps/web/src/utils/removeSuffix.ts
+++ apps/web/src/utils/removeSuffix.ts
@@ -1,7 +1,7 @@
 export const removeSuffixes = (formData: FormData, delimiter: string = "-«") => {
   const processedData = {} as Record<string, string>
 
-  for (const [key, value] in Array.from(formData.entries())) {
+  for (const [key, value] of Array.from(formData.entries())) {
     const [originalKey] = key.split(delimiter)
 
     processedData[originalKey] = value as string
